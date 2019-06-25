251171c564233f5fc856c001241d8137c89b785a
Chris DeVita <cdevita@yahoo.com>
2019-06-25 14:00:30 -0400

    Clean up run direcory

--- run/environs.sh
+++
@@ -1,6 +0,0 @@
-HOME="/home/pi/kplex-monitor"
-REPO="$HOME/repo"
-RUN="$HOME/run"
-LOG="$HOME/log"
-IINSTALL="$HOME/install"
-
--- run/kplex_server_simulator.sh
+++
@@ -1,2 +0,0 @@
- while (( 1 )) ; do cat ALL3.random | grep HC | sort | uniq  |( while read l ; do sleep 1; echo $l ; done )| nc 127.0.0.1 10111 ; done
-
